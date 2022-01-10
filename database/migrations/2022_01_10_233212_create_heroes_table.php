ed94e7fc677f180661b8bcfb94cdab8b3065387e
corsista <corsista@email.com>
2022-01-10 23:36:33 +0100

    aggiunta migration tabella heroes

---
+++ database/migrations/2022_01_10_233212_create_heroes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHeroesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('heroes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->unsignedInteger('energy');
+            $table->unsignedInteger('attack');
+            $table->unsignedInteger('defense');
+            $table->boolean('enabled')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('heroes');
+    }
+}
