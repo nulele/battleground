7e58ba4d606f4f509823cb86377015cd73c781fd
corsista <corsista@email.com>
2022-01-11 22:30:29 +0100

    aggiunta seeder Hero

---
+++ database/seeders/HeroSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Hero;
+use Illuminate\Database\Seeder;
+
+class HeroSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Hero::factory()
+            ->count(10)
+            ->create();
+    }
+}
