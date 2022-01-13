f531a7431977906ee8f7acd3645123a33f222b40
corsista <corsista@email.com>
2022-01-13 22:00:00 +0100

    create relazioni nei model Hero e Clan

--- app/Models/Clan.php
+++ app/Models/Clan.php
@@ -5,14 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Hero extends Model
+class Clan extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
 
-    public function clan()
+    public function heroes()
     {
-        return $this->belongsTo(Clan::class);
+        return $this->hasMany(Hero::class);
     }
 }
