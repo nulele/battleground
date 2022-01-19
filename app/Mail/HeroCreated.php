e994476cfaf7d3f48e08dc195d608e878240f476
corsista <corsista@email.com>
2022-01-19 23:49:51 +0100

    modifica classe email per invio tramite coda

--- app/Mail/HeroCreated.php
+++ app/Mail/HeroCreated.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class HeroCreated extends Mailable
+class HeroCreated extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
